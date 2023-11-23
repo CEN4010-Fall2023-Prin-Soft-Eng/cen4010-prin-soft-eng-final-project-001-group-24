39baea1852f8ee0019bbb5ff72c62f9b957ebc17
Forgi1 <112920565+Forgi1@users.noreply.github.com>
2023-11-23 17:41:26 -0500

    both login and sign up have been tested and now working

--- app.js
+++ app.js
@@ -1,43 +1,44 @@
-// auth.js
+document.addEventListener('DOMContentLoaded', function () {
+    Parse.initialize("qmiVcBHkyOi90FYFNs6r7e4J5beskXYTkqe85Qqm", "zgAXSRve1aW88Ck7dfO06emiorlN5KXmOrtYFuho");
+    Parse.serverURL = "https://parseapi.back4app.com/";
 
-Parse.initialize("qmiVcBHkyOi90FYFNs6r7e4J5beskXYTkqe85Qqm", "zgAXSRve1aW88Ck7dfO06emiorlN5KXmOrtYFuho");
-Parse.serverURL = "https://parseapi.back4app.com/";
+    document.getElementById('loginForm').addEventListener('submit', function (event) {
+        event.preventDefault();
 
-document.getElementById('loginForm').addEventListener('submit', function (event) {
-    event.preventDefault();
+        const username = document.getElementById('username').value;
+        const password = document.getElementById('password').value;
 
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
+        Parse.User.logIn(username, password).then((user) => {
+            console.log("User logged in:", user);
+            // Redirect to a new page after successful login
+            window.location.href = 'home.html'; // Change 'dashboard.html' to your desired page
+        }).catch((error) => {
+            console.error("Error logging in:", error);
+        });
+    });
 
-    Parse.User.logIn(username, password).then((user) => {
-        console.log("User logged in:", user);
-        // Redirect to a new page after successful login
-        window.location.href = 'home.html'; // Change 'dashboard.html' to your desired page
-    }).catch((error) => {
-        console.error("Error logging in:", error);
+    document.getElementById('signupForm').addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        const username = document.getElementById('username').value;
+        const email = document.getElementById('email').value;
+        const newPassword = document.getElementById('newPassword').value;
+
+        const user = new Parse.User();
+        user.set("username", username);
+        user.set("password", newPassword);
+        user.set("email", email);
+
+        user.signUp().then((user) => {
+            console.log("User signed up:", user);
+            // Redirect to a new page after successful sign-up
+            window.location.href = 'home.html'; // Change 'dashboard.html' to your desired page
+        }).catch((error) => {
+            console.error("Error signing up:", error);
+        });
     });
 });
 
-document.getElementById('signupForm').addEventListener('submit', function (event) {
-    event.preventDefault();
-
-    const username = document.getElementById('username').value;
-    const email = document.getElementById('email').value;
-    const newPassword = document.getElementById('newPassword').value;
-
-    const user = new Parse.User();
-    user.set("username", username);
-    user.set("password", newPassword);
-    user.set("email", email);
-
-    user.signUp().then((user) => {
-        console.log("User signed up:", user);
-        // Redirect to a new page after successful sign-up
-        window.location.href = 'home.html'; // Change 'dashboard.html' to your desired page
-    }).catch((error) => {
-        console.error("Error signing up:", error);
-    });
-});
 
 //Parse.initialize("qmiVcBHkyOi90FYFNs6r7e4J5beskXYTkqe85Qqm", "zgAXSRve1aW88Ck7dfO06emiorlN5KXmOrtYFuho");
 //github test
