d5b601e11a686fc5eeb9a3b14df40dc824cf8cfd
maxxparcesepe <mparcesepe2018@fau.edu>
2023-12-05 13:38:01 -0500

    Added More Functionality and Changed Port to 5678

--- FinalProjectServer.js
+++ FinalProjectServer.js
@@ -3,9 +3,11 @@ const bodyParser = require('body-parser');
 const Parse = require('parse/node');
 const swaggerJSDoc = require('swagger-jsdoc');
 const swaggerUI = require('swagger-ui-express');
+const fs = require('fs');
+const glob = require('glob');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5678;
 
 // Initialize Parse
 Parse.initialize("qmiVcBHkyOi90FYFNs6r7e4J5beskXYTkqe85Qqm", "zgAXSRve1aW88Ck7dfO06emiorlN5KXmOrtYFuho");
@@ -285,28 +287,6 @@ app.get('/listFavorites', (req, res) => {
 */
 
 /**
-* @swagger
-* /searchItems:
-*   get:
-*     tags:
-*       - Items
-*     summary: Search items
-*     parameters:
-*       - name: query
-*         in: query
-*         description: Search query
-*         required: true
-*         schema:
-*           type: string
-*     responses:
-*       200:
-*         description: Items matching the search query
-*       500:
-*         description: Error searching items
-*/
-
-
-/**
  * @swagger
  * /searchItems:
  *   get:
@@ -343,75 +323,182 @@ app.get('/searchItems', async (req, res) => {
     }
   });
   
-  /**
-   * @swagger
-   * /listItems:
-   *   get:
-   *     tags:
-   *       - Items
-   *     summary: List all items
-   *     responses:
-   *       200:
-   *         description: List of all items
-   *       500:
-   *         description: Error listing items
-   */
-  app.get('/listItems', async (req, res) => {
-    try {
-      // Example: Listing all items from a Parse class named 'Item'
-      const Item = Parse.Object.extend('Item');
-      const itemQuery = new Parse.Query(Item);
-      const items = await itemQuery.find();
+/**
+ * @swagger
+ * /listItems:
+ *   get:
+ *     tags:
+ *       - Items
+ *     summary: List all items
+ *     responses:
+ *       200:
+ *         description: List of all items
+ *       500:
+ *         description: Error listing items
+ */
+app.get('/listItems', async (req, res) => {
+  try {
+    // Example: Listing all items from a Parse class named 'Item'
+    const Item = Parse.Object.extend('Item');
+    const itemQuery = new Parse.Query(Item);
+    const items = await itemQuery.find();
+
+    const itemList = items.map(item => ({
+      id: item.id,
+      name: item.get('name'),  // Assuming there's a field named 'name' in your 'Item' class
+      date: item.get('date'),  // Assuming there's a field named 'date' in your 'Item' class
+      time: item.get('time'),  // Assuming there's a field named 'time' in your 'Item' class
+      // Add more fields as needed
+    }));
+
+    res.status(200).json(itemList);
+  } catch (error) {
+    console.error('Error listing items:', error);
+    res.status(500).send('Error listing items');
+  }
+});
+
   
-      res.json(items);
-    } catch (error) {
-      console.error('Error listing items:', error);
-      res.status(500).send('Error listing items');
+/**
+ * @swagger
+ * /getItemDetails/{itemId}:
+ *   get:
+ *     tags:
+ *       - Items
+ *     summary: Get item details
+ *     parameters:
+ *       - name: itemId
+ *         in: path
+ *         description: ID of the item
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Item details
+ *       404:
+ *         description: Item not found
+ *       500:
+ *         description: Error getting item details
+ */
+app.get('/getItemDetails/:itemId', async (req, res) => {
+  try {
+    const itemId = req.params.itemId;
+
+    // Example: Retrieving details for an item from a Parse class named 'Item'
+    const Item = Parse.Object.extend('Item');
+    const itemQuery = new Parse.Query(Item);
+    const item = await itemQuery.get(itemId);
+
+    if (!item) {
+      return res.status(404).json({ message: 'Item not found' });
     }
-  });
-  
-  /**
-   * @swagger
-   * /getItemDetails/{itemId}:
-   *   get:
-   *     tags:
-   *       - Items
-   *     summary: Get item details
-   *     parameters:
-   *       - name: itemId
-   *         in: path
-   *         description: ID of the item
-   *         required: true
-   *         schema:
-   *           type: string
-   *     responses:
-   *       200:
-   *         description: Item details
-   *       500:
-   *         description: Error getting item details
-   */
-  app.get('/getItemDetails/:itemId', async (req, res) => {
-    try {
-      const itemId = req.params.itemId;
-  
-      // Example: Retrieving details for an item from a Parse class named 'Item'
-      const Item = Parse.Object.extend('Item');
-      const itemQuery = new Parse.Query(Item);
-      const item = await itemQuery.get(itemId);
-  
-      res.json({
-        id: item.id,
-        name: item.get('name'),
-        description: item.get('description'),
+
+    const itemDetails = {
+      id: item.id,
+      name: item.get('name'),  
+      description: item.get('description'), 
+      date: item.get('date'),  
+      time: item.get('time'),  
+      
+    };
+
+    res.status(200).json(itemDetails);
+  } catch (error) {
+    console.error('Error getting item details:', error);
+    res.status(500).send('Error getting item details');
+  }
+});
+
+
+  // Function to check if a historical tour setup exists
+function checkTourSetupExists(identifier) {
+  const listOfSetups = glob.sync("historical_tours/*.json");
+
+  for (let i = 0; i < listOfSetups.length; i++) {
+    let setup = JSON.parse(fs.readFileSync(listOfSetups[i], 'utf8'));
+    if (setup.identifier === identifier) {
+      return true;
+    }
+  }
+  return false;
+}
+
+// Endpoint to create a new historical tour setup
+app.post('/createTourSetup', (req, res) => {
+  const identifier = new Date().toISOString(); // Using ISO date format as the identifier
+  const { date, time, name } = req.body;
+
+  const setup = {
+    identifier,
+    date,
+    time,
+    name,
+  };
+
+  const str = JSON.stringify(setup, null, 2);
+  const dir = 'historical_tours';
+
+  fs.access(dir, (err) => {
+    if (err) {
+      fs.mkdir(dir, (err) => {
+        if (err) {
+          console.error(err);
+        } else {
+          console.log('Directory created successfully!');
+        }
       });
-    } catch (error) {
-      console.error('Error getting item details:', error);
-      res.status(500).send('Error getting item details');
+    } else {
+      console.log('Directory already exists!');
+    }
+
+    if (!checkTourSetupExists(identifier)) {
+      fs.writeFile(`historical_tours/${identifier}.json`, str, (err) => {
+        if (err) {
+          return res.status(500).send({ message: 'Error creating tour setup' });
+        } else {
+          return res.status(201).send({ identifier, message: 'Tour setup created successfully' });
+        }
+      });
+    } else {
+      return res.status(200).send({ identifier, message: 'Tour setup already exists' });
     }
   });
-  
-  // Start the server
-  app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+// Endpoint to get details of a historical tour setup by identifier
+app.get('/getTourSetup/:identifier', (req, res) => {
+  const identifier = req.params.identifier;
+
+  fs.readFile(`historical_tours/${identifier}.json`, 'utf8', (err, data) => {
+    if (err) {
+      console.error('Error getting tour setup:', err);
+      return res.status(404).send({ message: 'Tour setup not found' });
+    } else {
+      const setup = JSON.parse(data);
+      return res.status(200).send(setup);
+    }
+  });
+});
+
+// Endpoint to list all historical tour setups
+app.get('/listTourSetups', (req, res) => {
+  const arr = [];
+  const files = glob.sync('historical_tours/*.json');
+
+  files.forEach((file) => {
+    const data = fs.readFileSync(file, 'utf8');
+    const setup = JSON.parse(data);
+    arr.push(setup);
   });
 
+  return res.status(200).send({ setups: arr });
+});
+
+//Start the server
+  app.listen(5678, () => {
+    console.log('Server is running...');
+    console.log('Webapp:   http://localhost:5678/');
+    console.log('API Docs: http://localhost:5678/api-docs');
+  });
+  
