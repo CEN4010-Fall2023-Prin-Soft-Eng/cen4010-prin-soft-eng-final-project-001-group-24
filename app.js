cb3684377c861fcac8ba59fd6051a292dd593b33
JoshuaF023 <JoshuaFrederick100@gmail.com>
2023-12-05 19:45:50 -0500

    Map API setup

--- app.js
+++ app.js
@@ -42,3 +42,28 @@ document.addEventListener('DOMContentLoaded', function () {
 
 //Parse.initialize("qmiVcBHkyOi90FYFNs6r7e4J5beskXYTkqe85Qqm", "zgAXSRve1aW88Ck7dfO06emiorlN5KXmOrtYFuho");
 //github test
+
+//just in case you want to use .env make sure to install the .env package
+//if not the api key is in the .env file for use.
+
+//Geoapify Key = GEOAPIFY_API_KEY=8e839545a466486ea818f2f8792eb92b
+
+require('dotenv').config(); // Load environment variables from a .env file
+
+const fetch = require('node-fetch');
+const apiKey = process.env.GEOAPIFY_API_KEY; // will take the api key from the .env file
+
+const requestOptions = {
+  method: 'GET',
+};
+
+const apiUrl = `https://api.geoapify.com/v2/place-details?id=514d368a517c511e40594bfd7b574ec84740f00103f90135335d1c00000000920313416e61746f6d697363686573204d757365756d&apiKey=${apiKey}`;
+
+fetch(apiUrl, requestOptions)
+  .then(response => response.json())
+  .then(result => {
+    // Process the result from the API
+    console.log("Place Details:", result);
+    // Perform additional actions based on the API response
+  })
+  .catch(error => console.log('Error:', error));
